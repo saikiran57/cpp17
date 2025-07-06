3c4ac0ef1c279eb4c0fa398960769445862ea521
Saikiran Nadipilli <saikirannadipilli@gmail.com>
2025-07-06 15:23:37 +0000

    restructure project

--- Basics/allocators/simple_allocator.cpp
+++ Basics/allocators/simple_allocator.cpp
@@ -68,29 +68,100 @@ public:
     }
 };
 
+class complex_widget
+{
+public:
+    complex_widget()
+    {
+        puts("complex_widget()");
+    }
+
+    ~complex_widget()
+    {
+        if (m_data)
+        {
+            delete m_data;
+            puts("~complex_widget() memory deleted");
+        }
+        else
+        {
+            puts("~complex_widget() no memory deleted");
+        }
+    }
+
+    complex_widget(int value) : m_data(new int(value))
+    {
+        puts("complex_widget(int)");
+    }
+
+    complex_widget(const complex_widget& other)
+    {
+        m_data = new int(*other.m_data);
+        puts("complex_widget(const complex_widget&)");
+    }
+
+    complex_widget& operator=(const complex_widget& other)
+    {
+        puts("complex_widget& =operator(const complex_widget&)");
+        if (this != &other)
+        {
+            delete m_data;
+            m_data = new int(*other.m_data);
+        }
+
+        return *this;
+    }
+
+    complex_widget(complex_widget&& other) noexcept : m_data(other.m_data)
+    {
+        other.m_data = nullptr;
+        puts("complex_widget(complex_widget&&)");
+    }
+
+    complex_widget& operator=(complex_widget&& other) noexcept
+    {
+        puts("complex_widget& =operator(complex_widget&&)");
+        if (this != &other)
+        {
+            delete m_data;
+            m_data = other.m_data;
+            other.m_data = nullptr;
+        }
+        return *this;
+    }
+
+private:
+    int* m_data = nullptr;
+};
+
 int main()
 {
-    SimpleAllocator<int> const alloc;
+    SimpleAllocator<complex_widget> const alloc;
+    std::vector<complex_widget, SimpleAllocator<complex_widget>> vec(alloc);
 
-    std::vector<int, SimpleAllocator<int>> vec(alloc);
+    // complex_widget w{1};
+    // vec.push_back(w);
+    // SimpleAllocator<int> const alloc;
 
-    std::cout << "Adding elements to vector:" << std::endl;
-    vec.push_back(1);
-    vec.push_back(2);
-    vec.push_back(3);
+    // std::vector<int, SimpleAllocator<int>> vec(alloc);
 
-    std::cout << "Vector contents: ";
-    for (int i : vec)
-    {
-        std::cout << i << " ";
-    }
-    std::cout << std::endl;
+    // std::cout << "Adding elements to vector:" << std::endl;
+    // vec.push_back(1);
+    // vec.push_back(2);
+    // vec.push_back(3);
 
-    std::cout << "Resizing vector to 1 element:" << std::endl;
-    vec.resize(1);
+    // std::cout << "Vector contents: ";
+    // for (int i : vec)
+    // {
+    //     std::cout << i << " ";
+    // }
+    // std::cout << std::endl;
 
-    std::cout << "Clearing vector:" << std::endl;
-    vec.clear();
+    // std::cout << "Resizing vector to 1 element:" << std::endl;
+    // vec.resize(1);
+
+    // std::cout << "Clearing vector:" << std::endl;
+    // vec.clear();
 
     return 0;
 }
---
+++ stl/containers/vector/README.md
@@ -0,0 +1,5 @@
+# Vector Concepts and Questions
+
+### Topics:
+
+- push_back vs emplace_back: When to use what [link](https://andreasfertig.com/blog/2023/04/push_back-vs-emplace_back-when-to-use-what/)
